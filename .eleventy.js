ca7fc792c6afb03c3d51fdd7329007d5c9d772f2
Jonathan Bobel <jonathan.bobel@gsa.gov>
2025-04-28 15:52:28 -0400

    config

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/images"); // ✅ Copy images to _site
   eleventyConfig.addPassthroughCopy("src/css");   // ✅ Serve CSS
   eleventyConfig.addPassthroughCopy("src/js");   // ✅ Serve js
+  eleventyConfig.addPassthroughCopy({ "src/favicon.ico": "favicon.ico" });
+  eleventyConfig.addPassthroughCopy({ "src/favicon.svg": "favicon.svg" });
+  eleventyConfig.addPassthroughCopy({ "src/favicon-96x96.png": "favicon-96x96.png" });
+  eleventyConfig.addPassthroughCopy({ "src/site.webmanifest": "site.webmanifest" });
 
   eleventyConfig.addCollection("projects", function(collectionApi) {
     return collectionApi.getFilteredByGlob("src/projects/*.md");
